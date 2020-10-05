4b4e0c1a22bc9d58af33e5eb2732a08a663e80f8
HJK9810 <ruby45342@gamail.com>
2020-10-05 15:02:58 +0900

    complete create two pages

--- 알약app/src/app/app.module.ts
+++ 알약app/src/app/app.module.ts
@@ -9,9 +9,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {NewonePage} from '../app/newone/newone.page';
+import {CountdownPage} from '../app/countdown/countdown.page'
+
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
+  declarations: [AppComponent, NewonePage, CountdownPage],
+  entryComponents: [NewonePage, CountdownPage],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
